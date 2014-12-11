7d0c371a51c680db428a818c31e6168411df74e3
Chris Griego <cgriego@gmail.com>
2014-12-11 11:22:54 -0600

    Redirect or render, there is no both

--- routes/games.js
+++ routes/games.js
@@ -3,6 +3,7 @@ var util = require("util");
 var express = require('express');
 var router = express.Router();
 var parseString = require('xml2js').parseString;
+var hbs = require('hbs');
 
 var bggRoot = "http://boardgamegeek.com/xmlapi2/thing?id=";
 
@@ -17,18 +18,20 @@ router.get('/:id', function (req, res) {
         var xmlResp = '';
         response.on('data', function (chunk) { xmlResp += chunk; });
         response.on('end', function () {
-            res.redirect('/');
             parseString(xmlResp, function (err, result) {
             console.log(util.inspect(result, false, null));
                 if (err) {
                     console.log(err.message);
                     res.redirect('/');
+                } else if (!result.items.item) {
+                    res.redirect('/');
+                } else {
+                    res.render('games', { game: {
+                        name: result.items.item[0].name[0].$.value,
+                        objectid: result.items.item[0].$.id,
+                        thumbnail: result.items.item[0].thumbnail
+                    }});
                 }
-                res.render('games', { game: {
-                    name: result.items.item[0].name[0].$.value,
-                    objectid: result.items.item[0].$.id,
-                    thumbnail: result.items.item[0].thumbnail
-                }});
             });
         });
     });
