b074e81fe877101da559a82a1445010e5b924061
David Stern <ergh99@yahoo.com>
2014-12-11 01:26:40 -0500

    more /games

--- routes/games.js
+++ routes/games.js
@@ -7,14 +7,14 @@ var bggRoot = "http://boardgamegeek.com/xmlapi2/thing?id=";
 
 /* GET game page. */
 router.get('/:id', function(req, res) {
-  console.log(req.params);
+  console.log(req.params.id);
   var request = http.get(bggRoot + req.params.id, function(response) {
     var xmlResp = '';
     response.on('data', function(chunk) { xmlResp += chunk; });
     response.on('end', function(err) {
       parseString(xmlResp, function (err, result) {
-        console.log(result);
-        res.render('games', { game: { name: result.items.item }});
+        console.log(util.inspect(result, false, null));
+		  res.render('games', { game: { name: result.items.item.name, thumbnail: result.items.item.thumbnail }});
 	  });
 	});
   });
