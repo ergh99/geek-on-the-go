4a9083110eb91a51e22755846b32bfa7c28d215c
David Stern <ergh99@yahoo.com>
2014-12-11 11:56:19 -0500

    much linting and error handling

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+//var users = require('./routes/users');
 var games = require('./routes/games');
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+//app.use('/users', users);
 app.use('/games', games);
 
 // catch 404 and forward to error handler
--- routes/games.js
+++ routes/games.js
@@ -7,17 +7,23 @@ var parseString = require('xml2js').parseString;
 var bggRoot = "http://boardgamegeek.com/xmlapi2/thing?id=";
 
 /* GET game page. */
-router.get('/:id', function(req, res) {
-  var request = http.get(bggRoot + req.params.id, function(response) {
-    var xmlResp = '';
-    response.on('data', function(chunk) { xmlResp += chunk; });
-    response.on('end', function(err) {
-      parseString(xmlResp, function (err, result) {
-//        console.log(util.inspect(result, false, null));
-        res.render('games', { game: { name: result.items.item[0].name[0].$.value, thumbnail: result.items.item[0].thumbnail }});
-	  });
-	});
-  });
+router.get('/:id', function (req, res) {
+    'use strict';
+    http.get(bggRoot + req.params.id, function (response) {
+        var xmlResp = '';
+        response.on('data', function (chunk) { xmlResp += chunk; });
+        response.on('end', function () {
+            res.redirect('/');
+            parseString(xmlResp, function (err, result) {
+            console.log(util.inspect(result, false, null));
+                if (err) {
+                    console.log(err.message);
+                    res.redirect('/');
+                }
+                res.render('games', { game: { name: result.items.item[0].name[0].$.value, thumbnail: result.items.item[0].thumbnail }});
+            });
+        });
+    });
 });
 
 module.exports = router;
