97890638deddff82d5bd88cfe9a4e5564017e1e0
David Stern <ergh99@yahoo.com>
2014-12-11 01:36:00 -0500

    remove xml2js dump

--- routes/games.js
+++ routes/games.js
@@ -8,14 +8,13 @@ var bggRoot = "http://boardgamegeek.com/xmlapi2/thing?id=";
 
 /* GET game page. */
 router.get('/:id', function(req, res) {
-  console.log(req.params.id);
   var request = http.get(bggRoot + req.params.id, function(response) {
     var xmlResp = '';
     response.on('data', function(chunk) { xmlResp += chunk; });
     response.on('end', function(err) {
       parseString(xmlResp, function (err, result) {
-        console.log(util.inspect(result, false, null));
-		  res.render('games', { game: { name: result.items.item.name, thumbnail: result.items.item.thumbnail }});
+//        console.log(util.inspect(result, false, null));
+        res.render('games', { game: { name: result.items.item[0].name[0].$.value, thumbnail: result.items.item[0].thumbnail }});
 	  });
 	});
   });
