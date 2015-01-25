6ec167ec5e7326553a1bce11fd694d0efb724218
Chris Griego <cgriego@gmail.com>
2015-01-24 18:19:17 -0600

    Working list addition

--- bgg.js
+++ bgg.js
@@ -24,6 +24,13 @@ function addGameToGeekList(bggCredentials, geeklistId, gameId, callback) {
   withAuthenticatedAgent(bggCredentials, function(agent, err, page) {
     agent.get({ uri: "http://boardgamegeek.com/geeklist/item/new/" + geeklistId + "/boardgame" }, function(err, page) {
       var form = page.form("ITEMFORM");
+
+      form.deleteField("action");
+      form.setFieldValue("action", "save");
+
+      form.deleteField("objecttype");
+      form.setFieldValue("objecttype", "thing");
+
       form.setFieldValue("objectid", gameId);
 
       form.submit(callback);
